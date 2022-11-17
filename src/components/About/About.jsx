e119dae5ec6cb011ce29b021c040afa57526cbc8
Sergey Shevchenko <98112335+serg-itsh@users.noreply.github.com>
2022-11-17 17:44:30 +0200

    Criated element tehnology

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,9 +4,45 @@ const About = () => {
   return (
     <div className={styles.about}>
       <div className={styles.container}>
-        <h1>About</h1>
-        <ul>
-          <li></li>
+        {/* <h1>About</h1> */}
+        <ul className={styles.about_wrapper}>
+          <li>
+            <h2>SUMMARY</h2>
+            <p className={styles.text}>
+              I am a Front-End developer looking for a full-time position in a
+              company that's aimed at making both everyday life and business
+              easier, more productive and user-friendly. I have sound knowledge
+              of HTML, CSS, JavaScript, and React. I am a fast learner,
+              responsible, and ready for challenging tasks .
+            </p>
+          </li>
+          <li>
+            <h2>WORK EXPERIENCE</h2>
+            <p className={styles.text}>
+              <strong>Mechanic</strong> <br/>Cardboard and paper factory "Papir-Mal" | 2013-2022 Task
+              coordination planning and final quality control of completed
+              works. Development of drawings and design of machine parts.
+              Installation of a cardboard machine. Modernization of equipment.
+            </p>
+          </li>
+          <li>
+            <h2>EDUCATION</h2>
+            <p className={styles.text}>
+              National Technical Univer sity of Ukraine "Kyiv Polytechnic
+              Institute" 2006 - 2012 Mechanical engineer, specialty "Technology
+              of machine-building"
+            </p>
+            <h2>ADDITIONAL EDUCATION</h2>
+            <p className={styles.text}>
+              <strong>IT School GoIT (Kyiv)</strong><br/>
+               2022 Full Stack Developer
+               
+            </p>
+            <p className={styles.text}>
+            IT School
+              Prometheus (Lviv) 2021 Bas i c Software Tes ting
+            </p>
+          </li>
         </ul>
       </div>
     </div>
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,48 +9,103 @@ const Portfolio = () => {
     <div className={styles.portfolio}>
       <div className={styles.container}>
         <h1 className={styles.title}>Portfolio team projects</h1>
-        
-
-     
 
         <ul className={styles.portfolioBox}>
-
-        <li className={styles.pictures}> 
-        <p className={styles.text}>PRO-TEST</p>                       
+          <li className={styles.pictures}>
+            <p className={styles.text}>PRO-TEST</p>
             <img
-            className={styles.picturesImg}
+              className={styles.picturesImg}
               src={Project_4}
               alt="project_pro-test"
             />
+            <span className={styles.overlay}></span>
           </li>
 
-        <li className={styles.pictures}>  
-        <p className={styles.text}>FILMOTEKA</p>                      
+          <li className={styles.pictures}>
+            <p className={styles.text}>FILMOTEKA</p>
             <img
-            className={styles.picturesImg}
+              className={styles.picturesImg}
               src={Project_3}
               alt="project_filmoteka"
             />
           </li>
 
-        <li className={styles.pictures}> 
-        <p className={styles.text}>ICECREAM</p>                       
+          <li className={styles.pictures}>
+            <p className={styles.text}>ICECREAM</p>
             <img
-            className={styles.picturesImg}
+              className={styles.picturesImg}
               src={Project_2}
               alt="project_icecream"
             />
           </li>
 
-          <li className={styles.pictures}>  
-          <p className={styles.text}>WEB-STUDIO <br/> Own project</p>                      
+          <li className={styles.pictures}>
+            <p className={styles.text}>
+              WEB-STUDIO
+              <br />
+              -Own project
+            </p>
             <img
-            className={styles.picturesImg}
+              className={styles.picturesImg}
               src={Project_1}
               alt="project_web-studio"
             />
           </li>
         </ul>
+        <div>
+          <h2>Use of technologies on projects.</h2>
+          <div className={styles.icon}>
+
+          <div className={styles.iconScale}>
+                <img 
+                
+                src="https://i.pinimg.com/originals/5e/fd/4e/5efd4e04173b52c1c4d1f459679bf7fb.png" 
+                alt="figma_logo"  
+                width={50} 
+                // height={100}
+                /> 
+              </div>
+           
+              <div className={styles.iconScale}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2048px-HTML5_logo_and_wordmark.svg.png" 
+                alt="HTML5_logo"  
+                width={50} 
+                // height={24}
+                /> 
+              </div>
+              <div className={styles.iconScale}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png" 
+                alt="CSS3_logo"  
+                width={50} 
+                // height={24}
+                /> 
+              </div>
+              <div className={styles.iconScale}>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascript-shield.svg/1200px-Javascript-shield.svg.png" 
+                alt="Javascript_logo"  
+                width={50} 
+                height={50}
+                /> 
+              </div>
+              <div className={styles.iconScale}>
+                <img src="https://camo.githubusercontent.com/c38bf4a44750bd9b576a2259a5074dd277d63f0a412b5b1f31f54e516711ef5b/687474703a2f2f736173732d6c616e672e636f6d2f6173736574732f696d672f7374796c6567756964652f7365616c2d636f6c6f722d61656630333534632e706e67" 
+                alt="sass_logo"  
+                // width={24} 
+                // height={24}
+                /> 
+              </div>
+              
+              <div className={styles.iconScale}>
+                <img src="https://icons-for-free.com/iconfiles/png/512/design+development+facebook+framework+mobile+react+icon-1320165723839064798.png" 
+                alt="react_logo"  
+                width={100} 
+                // height={24}
+                /> 
+              </div>
+            
+          
+          </div>
+        </div>
       </div>
     </div>
   );
